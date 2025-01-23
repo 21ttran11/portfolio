a3f0feda46d4cf8d348391e8acd01bf34cb1a32f
21ttran11 <58841978+21ttran11@users.noreply.github.com>
2025-01-23 12:01:51 -0800

    links

--- src/Header.jsx
+++ src/Header.jsx
@@ -5,11 +5,13 @@ const Header = () => (
   <div className="header-container">
     <div className="header-text">
       <p>
-      <span style={{ fontSize: '37px' }}>Tiffany Tran</span> is a <span style={{ fontWeight: '450' }} className="yellow">game programmer</span> passionate about creativity and <span className="blue">leadership</span>. 
+      <span style={{ fontSize: '38px' }}>Tiffany Tran</span> is a <span style={{ fontWeight: '450' }} className="yellow">game programmer</span> passionate about creativity and <span className="blue">leadership</span>. 
         Currently studying computer science at California State University of Fullerton, she is dedicated to <span className="red">fostering community</span> and <span className="green">connecting players</span> through developing and enhancing immersive gameplay. 
       </p>
-      <a href="more.html" className="more">more</a>
     </div>
+      <a href="more.html" className="more">
+        more →
+      </a>
   </div>
 </header>
 );
--- src/Links.jsx
+++ src/Links.jsx
@@ -2,11 +2,16 @@ import React from 'react';
 import './index.css'; // Ensure you include your CSS file here
 
 const Links = () => (
-  <div className="links">
-    <a href="https://github.com/21ttran11" target="_blank" rel="noopener noreferrer">GitHub</a>
-    <a href="https://pomjellies.itch.io/" target="_blank" rel="noopener noreferrer">Itch.io</a>
-    <a href="https://www.linkedin.com/in/tiffany-tran-8280812bb/" target="_blank" rel="noopener noreferrer">LinkedIn</a>
+  <div className="floating-box">
+    <p> Links </p>
+    <hr class="divider"/>
+    <div class="links">
+      <a href="https://github.com" target="_blank">GitHub</a><br />
+      <a href="https://linkedin.com" target="_blank">LinkedIn</a> <br/>
+      <a href="https://itch.io" target="_blank">Itch.io</a>
+    </div>
   </div>
+  
 );
 
 export default Links;
